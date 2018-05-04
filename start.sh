ecca62ed0ce58cf40f956c5f977a6673e91d417e
Lucas Savva <lucas@vccomputers.ie>
2018-05-04 01:43:06 +0100

    Add start script

---
+++ start.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+screen -dmS craig_gmod ./srcds_run -game garrysmod -maxplayers 12 -ip 0.0.0.0 +host_workshop_collection 1366706246 +gamemode terrortown +map ttt_rooftops_2016_v1
